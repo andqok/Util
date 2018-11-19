106cbd395566a6d0c0860981952f91aa845e4e1d
Andrew Domanski <andqok@gmail.com>
2018-11-19 06:49:31 +0200

    remove obsolete function in util-rand, rename util-safely-add to util-safely-set

--- util-rand.js
+++ util-rand.js
@@ -7,20 +7,15 @@ rand.arrayIndex = function (arr) {
     return Math.floor(Math.random() * arr.length)
 }
 rand.arrayEl.nonMutating = function (arr) {
-  return arr[rand.arrayIndex(arr)]
+    const index = rand.arrayIndex(arr)
+    return arr[index]
 }
 rand.arrayEl.mutating = function randElCut(arr) {
-  const index = rand.arrayIndex(arr)
-  const out = arr[index]
-  arr.splice(index, 1)
-  return out
+    const index = rand.arrayIndex(arr)
+    const out = arr[index]
+    arr.splice(index, 1)
+    return out
 }
 rand.objectProperty = function (obj) {
     return rand.arrayEl.nonMutating(Object.keys(obj))
-}
-
-function presetBranch(trunk, branch, value) {
-    if (is.emptyVar(trunk[branch])) {
-        trunk[branch] = value
-    }
 }
---
+++ util-safely-set.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const safelySet = {}
+
+safelySet.number = function (tree, newBranch, number) {
+    if (!tree.hasOwnProperty(newBranch)) {
+        tree[newBranch] = number
+    } else {
+        tree[newBranch] += number
+    }
+}
+
+safelySet.array = function (tree, newBranch, item) {
+    if (!tree.hasOwnProperty(newBranch)) {
+        
+    }
+}
+
